20871c45657d384f4ed57091a223caf59268bd82
Uladzimir Horbach <uhorbach@scnsoft.com>
2025-03-20 23:57:48 +0300

    Init

---
+++ Host/SmartCactusHost/SCTelegramBot/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SCTelegramBot;
+
+internal class Program
+{
+    static string? _apiKey;
+    static void Main(string[] args)
+    {
+        InitVariables();
+    }
+
+    static void InitVariables()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+        _apiKey = configuration["TelegramBotApiKey"]?.ToString() ??
+            throw new Exception("TelegramBotApiKey not found");
+    }
+}
