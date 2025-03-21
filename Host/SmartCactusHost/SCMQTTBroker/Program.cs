97790b2c8c708932d04f2a2fe179396c81dafe3c
Uladzimir Horbach <uhorbach@scnsoft.com>
2025-03-21 23:40:18 +0300

    telegram bot and mqtt init

---
+++ Host/SmartCactusHost/SCMQTTBroker/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.Reflection;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+
+namespace SCMQTTBroker;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var certificatePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "certificate.pfx");
+        var certificatePassword = "hello";
+
+        if (!File.Exists(certificatePath))
+        {
+            Console.WriteLine($"Sertificate not found: {certificatePath}");
+            return;
+        }
+
+        var certificate = new X509Certificate2(certificatePath, certificatePassword, X509KeyStorageFlags.Exportable);
+
+        var mqttServerOptions = new MqttServerOptionsBuilder()
+            .WithoutDefaultEndpoint()
+            .WithEncryptedEndpoint()
+            .WithEncryptedEndpointPort(8883)
+            .WithEncryptionCertificate(certificate.Export(X509ContentType.Pfx))
+            .WithEncryptionSslProtocol(SslProtocols.Tls12)
+            .Build();
+
+        var mqttFactory = new MqttServerFactory();
+        var mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions);
+        mqttServer.ValidatingConnectionAsync += e =>
+        {
+            if (string.IsNullOrEmpty(e.ClientId))
+            {
+                e.ReasonCode = MqttConnectReasonCode.ClientIdentifierNotValid;
+            }
+            else
+            {
+                e.ReasonCode = MqttConnectReasonCode.Success;
+            }
+            return Task.CompletedTask;
+        };
+        mqttServer.ApplicationMessageEnqueuedOrDroppedAsync += e =>
+        {
+            var a = e.ApplicationMessage.Payload.ToArray();
+            var b= string.Concat(a.Select(x => (char)x));
+
+            Console.WriteLine($"[{DateTime.Now}] Payload {b} for Client: {e.ReceiverClientId}, from: {e.SenderClientId}, Topic: {e.ApplicationMessage.Topic}");
+            return Task.CompletedTask;
+        };
+
+        Console.WriteLine("Starting...");
+        await mqttServer.StartAsync();
+
+
+        Console.WriteLine("Broker started. Pres any key to exit...");
+
+        Console.ReadKey();
+
+        await mqttServer.StopAsync();
+        Console.WriteLine("Broker finished.");
+    }
+}
--- Host/SmartCactusHost/SCTelegramBot/Program.cs
+++ Host/SmartCactusHost/SCTelegramBot/Program.cs
@@ -1,13 +1,105 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot.Polling;
+using Telegram.Bot;
+using static Telegram.Bot.TelegramBotClient;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace SCTelegramBot;
 
 internal class Program
 {
-    static string? _apiKey;
-    static void Main(string[] args)
+    private static ITelegramBotClient? _botClient;
+    private static ReceiverOptions? _receiverOptions;
+    private static string? _apiKey;
+
+    static async Task Main()
     {
         InitVariables();
+     
+        _botClient = new TelegramBotClient(_apiKey ?? "");
+        _receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates =
+            [
+                UpdateType.Message,
+                UpdateType.CallbackQuery,
+            ],
+            DropPendingUpdates = true,
+        };
+
+        using var cts = new CancellationTokenSource();
+
+        _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token);
+
+        var me = await _botClient.GetMe();
+        Console.WriteLine($"{me.Username} started!");
+
+        await Task.Delay(-1);
+    }
+
+    private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        try
+        {
+            switch (update.Type)
+            {
+                case UpdateType.Message:
+                {
+                        var message = update.Message;
+                        if (message is null || message.Type != MessageType.Text)
+                            return;
+                        var user = message.From;
+                        Console.WriteLine($"{user.Username} ({user.Id}) wrote msg: {message.Text}");
+
+                        var chat = message.Chat;
+
+                        var inlineKeyboard = new InlineKeyboardMarkup(
+                                    new List<InlineKeyboardButton[]>()
+                                    {
+                                        new InlineKeyboardButton[]
+                                        {
+                                            InlineKeyboardButton.WithCallbackData("button1", "button1"),
+                                            InlineKeyboardButton.WithCallbackData("button2", "button2"),
+                                        },
+                                        new InlineKeyboardButton[]
+                                        {
+                                            InlineKeyboardButton.WithCallbackData("button3", "button3"),
+                                            InlineKeyboardButton.WithCallbackData("button4", "button4"),
+                                            InlineKeyboardButton.WithCallbackData("button5", "button5"),
+                                        },
+                                    }
+                            );
+
+                        await botClient.SendMessage(
+                            chat.Id,
+                            message.Text, 
+                            replyParameters: new ReplyParameters() { MessageId = message.MessageId },
+                            replyMarkup: inlineKeyboard);
+
+                        return;
+                    }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    private static Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken)
+    {
+        var ErrorMessage = error switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => error.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
     }
 
     static void InitVariables()
