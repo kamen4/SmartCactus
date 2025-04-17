669f4ff6fa169608ed1f1b1f8e5b5581f1277875
Uladzimir Horbach <uhorbach@scnsoft.com>
2025-04-17 21:47:52 +0300

    repository architecture

---
+++ Host/SmartCactusHost/Entities/Enums/UserRole.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Enums;
+
+public enum UserRole
+{
+    User,
+    Admin,
+}
---
+++ Host/SmartCactusHost/Entities/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Entities.Enums;
+
+namespace Entities.Models;
+
+public class User
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string? TelegramId { get; set; }
+    public UserRole Role { get; set; }
+}
